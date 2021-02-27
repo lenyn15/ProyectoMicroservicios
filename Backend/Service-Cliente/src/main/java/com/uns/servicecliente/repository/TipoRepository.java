9824054b2fb1c223ddda0632ca63ad7d422d1254
lenyn15 <201514029@uns.edu.pe>
2021-02-27 00:26:22 -0500

    Code

--- Backend/Service-Cliente/src/main/java/com/uns/servicecliente/repository/TipoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.uns.servicecliente.repository;
-
-import com.uns.servicecliente.entity.Tipo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TipoRepository extends JpaRepository<Tipo, Integer> {
-}
