741a1ae483910bfb9781ea69e3604536db12f388
lenyn15 <201514029@uns.edu.pe>
2021-02-26 22:17:46 -0500

    Proyecto

---
+++ Backend/Service-Cliente/src/main/java/com/uns/servicecliente/ServiceClienteApplication.java
@@ -0,0 +1,14 @@
+package com.uns.servicecliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceClienteApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( ServiceClienteApplication.class,
+		                       args );
+	}
+
+}
---
+++ Backend/Service-Cliente/src/test/java/com/uns/servicecliente/ServiceClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uns.servicecliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
