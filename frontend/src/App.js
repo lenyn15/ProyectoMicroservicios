10019ac81966511b0d8422f82addfb842663b308
lenyn15 <201514029@uns.edu.pe>
2021-02-27 02:50:21 -0500

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Menu                               from "./menuOpciones/menu";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Menu/>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/menuOpciones/menu.jsx
@@ -0,0 +1,50 @@
+import "./menu.scss"
+import React, { Component }                             from "react";
+import { FontAwesomeIcon }                              from "@fortawesome/react-fontawesome";
+import { faAppStore }                                   from "@fortawesome/free-brands-svg-icons";
+import { faBriefcase, faShoppingCart, faStore, faUser } from "@fortawesome/free-solid-svg-icons";
+
+class Menu extends Component {
+    constructor( props ) {
+        super( props );
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="l-navbar" id="navbar">
+                <nav className="nav">
+                    <div>
+                        <div className="nav__logo">
+                            <i className="icon"><FontAwesomeIcon icon={ faAppStore }/></i>
+                            <span className="nav__logo-text">Tienda Sidecom</span>
+                        </div>
+                        <ul className="nav__list">
+                            <a href={ "/component-ventas" } className="nav__link">
+                                <i className="nav__icon"><FontAwesomeIcon icon={ faStore }/></i>
+                                <span className="nav__text">Ventas</span>
+                            </a>
+
+                            <a href={ "/product-list" } className="nav__link">
+                                <i className="nav__icon"><FontAwesomeIcon icon={ faShoppingCart }/></i>
+                                <span className="nav__text">Productos</span>
+                            </a>
+
+                            <a href={ "/component-empleados" } className="nav__link">
+                                <i className="nav__icon"><FontAwesomeIcon icon={ faBriefcase }/></i>
+                                <span className="nav__text">Empleados</span>
+                            </a>
+
+                            <a href={ "/component-clientes" } className="nav__link">
+                                <i className="nav__icon"><FontAwesomeIcon icon={ faUser }/></i>
+                                <span className="nav__text">Clientes</span>
+                            </a>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Menu
