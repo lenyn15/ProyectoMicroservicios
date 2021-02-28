08b4524914fd1652f15fdab88c8957d37e8a3532
lenyn15 <201514029@uns.edu.pe>
2021-02-28 01:08:06 -0500

    Service Sales

---
+++ Backend/Service-Venta/src/main/java/com/uns/serviceventas/ServiceVentasApplication.java
@@ -0,0 +1,14 @@
+package com.uns.serviceventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceVentasApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( ServiceVentasApplication.class,
+		                       args );
+	}
+
+}
