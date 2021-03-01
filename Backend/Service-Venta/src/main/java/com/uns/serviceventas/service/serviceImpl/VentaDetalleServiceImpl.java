95f5978da0256d0ac93e4c7d77e8ef90ada6befd
lenyn15 <201514029@uns.edu.pe>
2021-03-01 10:33:59 -0500

    Service Sales

---
+++ Backend/Service-Venta/src/main/java/com/uns/serviceventas/service/serviceImpl/VentaDetalleServiceImpl.java
@@ -0,0 +1,80 @@
+package com.uns.serviceventas.service.serviceImpl;
+
+import com.uns.serviceventas.client.FeignCliente;
+import com.uns.serviceventas.client.FeignProducto;
+import com.uns.serviceventas.entity.VentaDetalle;
+import com.uns.serviceventas.model.Cliente;
+import com.uns.serviceventas.model.Producto;
+import com.uns.serviceventas.repository.VentaDetalleRepository;
+import com.uns.serviceventas.service.VentaDetalleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VentaDetalleServiceImpl implements VentaDetalleService {
+	
+	private final VentaDetalleRepository ventaDetalleRepository;
+	private final FeignCliente           feignCliente;
+	private final FeignProducto          feignProducto;
+	
+	@Autowired
+	public VentaDetalleServiceImpl( VentaDetalleRepository ventaDetalleRepository,
+	                                FeignCliente feignCliente,
+	                                FeignProducto feignProducto ) {
+		this.ventaDetalleRepository = ventaDetalleRepository;
+		this.feignCliente           = feignCliente;
+		this.feignProducto          = feignProducto;
+	}
+	
+	@Override
+	public List<VentaDetalle> listAll() {
+		return ventaDetalleRepository.findAll();
+	}
+	
+	@Override
+	public VentaDetalle findByID( Integer idVentaDetalle ) {
+		VentaDetalle detalle = ventaDetalleRepository.findById( idVentaDetalle ).orElse( null );
+		if ( detalle != null ) {
+			Producto producto = feignProducto.buscarProducto( detalle.getProducto().getId() ).getBody();
+			detalle.setProducto( producto );
+		}
+		return detalle;
+	}
+	
+	@Override
+	public VentaDetalle addNew( VentaDetalle ventaDetalle ) {
+		return ventaDetalleRepository.saveAndFlush( ventaDetalle );
+	}
+	
+	@Override
+	public VentaDetalle update( VentaDetalle ventaDetalle ) {
+		return ventaDetalleRepository.saveAndFlush( ventaDetalle );
+	}
+	
+	@Override
+	public void delete( Integer idVentaDetalle ) {
+		ventaDetalleRepository.deleteById( idVentaDetalle );
+	}
+	
+	@Override
+	public List<Cliente> listarClientes() {
+		return feignCliente.listarClientes().getBody();
+	}
+	
+	@Override
+	public List<Cliente> filtrarClientes( String nombre ) {
+		return feignCliente.filtrarClientes( nombre ).getBody();
+	}
+	
+	@Override
+	public List<Producto> listarProductos() {
+		return feignProducto.listarProductos().getBody();
+	}
+	
+	@Override
+	public List<Producto> filtrarProductos( String nombre ) {
+		return feignProducto.filtrarPorNombre( nombre ).getBody();
+	}
+}
